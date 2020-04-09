eb35f5d55c5d0c3f7f9e3bf34ab1a7e7b2de8868
SOKOL13UKR@UKR.NET <SOKOL13UKR@UKR.NET>
2020-04-09 12:18:40 +0300

    add

--- src/java/part2/Amusement.java
+++ src/java/part2/Amusement.java
@@ -5,11 +5,15 @@ public class Amusement {
     private  int age;
     private  String place;
     private float complexity;
+    private Float noisiness;
+    private String inventory;
+
 
     public void setAge(int age) {
         this.age = age;
     }
     public void setComplexity(float complexity) { this.complexity = complexity;}
+    public void setPrice(float price){this.price = price;}
 
     private float getPrice() {
         return price;
@@ -33,8 +37,27 @@ public class Amusement {
     public void сhangePriceDependOnPlace(String place){
        if(place == "beautiful")
            price = price*2;
-
     }
+    public void MoreInventoryMorePrice (String inventory){
+        if(inventory=="a lot of")
+            price = price * 3;
+    }
+    public void LessInventoryLessNoisiness(String inventory){
+        if(inventory == "little inventory")
+            noisiness = noisiness - 10;
+    }
+   public void MoreComplexityLessAge (float complexity){
+        if(complexity==(+10))
+            age= age + 20;
+   }
+   public static void main(String[] args){
+       Amusement tennis = new Amusement();
+       tennis.setPrice(1000);
+       tennis.setAge(10);
 
+
+       System.out.println("Price and Age :" +" " + tennis.getPrice()  + "and" + tennis.getAge() );
+
+   }
 
 }
